9ca019393458639bdbebb9cc07b6d161662eaa13
SystemTOGA <gentleman@SystemTOGAs-MacBook-Pro-5.local>
2012-05-23 03:40:07 +0900

    Initial Commit

---
+++ Place Melody/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  Place Melody
+//
+//  Created by  on 5/23/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
